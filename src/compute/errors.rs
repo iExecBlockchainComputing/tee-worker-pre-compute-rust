fafbc54e6c2dfd0563ef8d5c02ad1e9366a7753a
nabil-Tounarti <nabil.tounarti0@gmail.com>
2025-05-21 11:29:20 +0200

    feat: implement environment arguments handling

--- src/compute/errors.rs
+++ src/compute/errors.rs
@@ -5,10 +5,26 @@ use thiserror::Error;
 #[serde(rename_all(serialize = "SCREAMING_SNAKE_CASE"))]
 #[allow(clippy::enum_variant_names)]
 pub enum ReplicateStatusCause {
+    #[error("At least one input file URL is missing")]
+    PreComputeAtLeastOneInputFileUrlMissing,
+    #[error("Dataset checksum related environment variable is missing")]
+    PreComputeDatasetChecksumMissing,
+    #[error("Dataset filename related environment variable is missing")]
+    PreComputeDatasetFilenameMissing,
+    #[error("Dataset key related environment variable is missing")]
+    PreComputeDatasetKeyMissing,
+    #[error("Dataset URL related environment variable is missing")]
+    PreComputeDatasetUrlMissing,
     #[error("Unexpected error occurred")]
     PreComputeFailedUnknownIssue,
     #[error("Invalid TEE signature")]
     PreComputeInvalidTeeSignature,
+    #[error("IS_DATASET_REQUIRED environment variable is missing")]
+    PreComputeIsDatasetRequiredMissing,
+    #[error("Input files number related environment variable is missing")]
+    PreComputeInputFilesNumberMissing,
+    #[error("Output path related environment variable is missing")]
+    PreComputeOutputPathMissing,
     #[error("Task ID related environment variable is missing")]
     PreComputeTaskIdMissing,
     #[error("TEE challenge private key related environment variable is missing")]
