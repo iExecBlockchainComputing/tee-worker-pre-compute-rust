9f5a45cc7c7ec2375e5d4f867950d35c623aa13e
nabil-Tounarti <117689544+nabil-Tounarti@users.noreply.github.com>
2025-07-07 17:47:47 +0200

    feat: improve logger initialization to set a default log level (#20)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use env_logger::Env;
 use std::process;
 
 mod api;
 mod compute;
 
 fn main() {
-    env_logger::init();
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     process::exit(compute::app_runner::start());
 }
