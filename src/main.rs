f49adf0e74613804994a6f0a501e0dd285692edb
nabil-Tounarti <nabil.tounarti0@gmail.com>
2025-07-07 17:25:13 +0200

    feat: set default log level to info if RUST_LOG is not set

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::process;
 
+use env_logger::Env;
+
 mod api;
 mod compute;
 
 fn main() {
-    env_logger::init();
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     process::exit(compute::app_runner::start());
 }
